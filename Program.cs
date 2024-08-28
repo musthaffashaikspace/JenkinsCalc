be049b9b043a4def2023c95e33bea7aac7b2eddc
Musthaffa Shaik <musthaffa.shaik@planview.com>
2024-08-28 11:56:10 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MyApp;
+
+Console.WriteLine("Hello, World!");
+
+Calculator calculator = new Calculator();
+Console.WriteLine("The Output of addtion is ");
+Console.WriteLine(calculator.Add(10, 10));
